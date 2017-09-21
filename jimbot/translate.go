4459c004c66f2d5262b3a69376e1ae7f5456c3a8
jm33-m0 <jm33.m0@gmail.com>
2017-09-21 23:20:45 +0800

    google translate, init

--- jimbot/translate.go
+++ jimbot/translate.go
@@ -1,1 +1,31 @@
 package jimbot
+
+import (
+	//"github.com/buger/jsonparser"
+	"io/ioutil"
+	"log"
+	"net/http"
+)
+
+const (
+	url = "https://translate.googleapis.com/translate_a/single?client=gtx&sl=auto&tl=en&dt=t&q="
+)
+
+// ToEnglish : translate any language to English
+func ToEnglish(text string) string {
+	var textTrans string
+	queryURL := url + text
+
+	req, err := http.Get(queryURL)
+	if err != nil {
+		log.Print("[*] Can't reach translate API")
+	}
+	defer req.Body.Close()
+	readBody, _ := ioutil.ReadAll(req.Body)
+	log.Print("[+++] TRANSLATE response: ", readBody)
+
+	//data, _, _, _  := jsonparser.Get(readBody, [0][0][0])
+
+	textTrans = "Test translate"
+	return textTrans
+}
