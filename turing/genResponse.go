3e67b531d445a9ae9eb4bb58196499c8e942650d
jm33-m0 <jm33.m0@gmail.com>
2018-09-03 00:02:05 +0800

    gofmt

--- turing/genResponse.go
+++ turing/genResponse.go
@@ -15,26 +15,26 @@ const apiURL = "http://openapi.tuling123.com/openapi/api/v2"
 func GetResponse(input string) string {
 	// json data to post
 	data := []byte(fmt.Sprintf(`{
-	"reqType":0,
-    "perception": {
-        "inputText": {
-            "text": "%s"
-        },
-        "inputImage": {
-            "url": ""
-        },
-        "selfInfo": {
-            "location": {
-                "city": "",
-                "province": "",
-                "street": ""
-            }
-        }
-    },
-    "userInfo": {
-        "apiKey": "063487b9cffb41adbebe25c86b56b807",
-        "userId": "jimdeb3"
-    }}`, input))
+		"reqType":0,
+		"perception": {
+			"inputText": {
+				"text": "%s"
+			},
+			"inputImage": {
+				"url": ""
+			},
+			"selfInfo": {
+				"location": {
+					"city": "",
+					"province": "",
+					"street": ""
+				}
+			}
+		},
+		"userInfo": {
+			"apiKey": "063487b9cffb41adbebe25c86b56b807",
+			"userId": "jimdeb3"
+		}}`, input))
 
 	req, err := http.NewRequest("POST", apiURL, bytes.NewBuffer(data))
 	// req.Header.Set("X-Custom-Header", "myvalue")
