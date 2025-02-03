27d2413c7d0bce7959d1f82e5e17fb0fb4173c91
jm33-m0 <jm33@jm33.me>
2025-02-03 18:08:35 +1100

    use ollama for chatbot

--- turing/genResponse.go
+++ turing/genResponse.go
@@ -2,48 +2,30 @@ package turing
 
 import (
 	"bytes"
+	"encoding/json" // added import
 	"fmt"
-	"io/ioutil"
+	"io"
 	"log"
 	"net/http"
-
-	"github.com/buger/jsonparser"
 )
 
-const apiURL = "http://openapi.tuling123.com/openapi/api/v2"
+const apiURL = "http://127.0.0.1:11434/chat" // updated endpoint
 
-// GetResponse : get response from turing api
+// GetResponse : get response from ollama server
 func GetResponse(input string) string {
-	// json data to post
-	data := []byte(fmt.Sprintf(`{
-		"reqType":0,
-		"perception": {
-			"inputText": {
-				"text": "%s"
-			},
-			"inputImage": {
-				"url": ""
-			},
-			"selfInfo": {
-				"location": {
-					"city": "",
-					"province": "",
-					"street": ""
-				}
-			}
-		},
-		"userInfo": {
-			"apiKey": "063487b9cffb41adbebe25c86b56b807",
-			"userId": "jimdeb3"
-		}}`, input))
+	// Updated payload with prompt field for Ollama
+	data := []byte(fmt.Sprintf(`{"prompt": "%s"}`, input))
 
 	req, err := http.NewRequest("POST", apiURL, bytes.NewBuffer(data))
+	if err != nil {
+		return fmt.Sprintf("Could not reach Ollama server: %v", err)
+	}
 	req.Header.Set("Content-Type", "application/json")
 
 	client := &http.Client{}
 	resp, err := client.Do(req)
 	if err != nil {
-		return "Could not reach Turing API"
+		return fmt.Sprintf("Could not reach Ollama server: %v", err)
 	}
 
 	defer func() {
@@ -52,8 +34,16 @@ func GetResponse(input string) string {
 			log.Println(err)
 		}
 	}()
-	responseRaw, _ := ioutil.ReadAll(resp.Body)
+	responseRaw, _ := io.ReadAll(resp.Body)
 
-	response, _ := jsonparser.GetString(responseRaw, "results", "[0]", "values", "text")
-	return response
+	// Define a struct to parse the JSON response using stdlib
+	var res struct {
+		Response string `json:"response"`
+	}
+
+	err = json.Unmarshal(responseRaw, &res)
+	if err != nil {
+		return "Failed to parse response"
+	}
+	return res.Response
 }
